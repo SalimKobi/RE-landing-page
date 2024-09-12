2bff8dfe0fee28bc3d76aa74526d89107edd1f08
Salim Kobi <84938486+SalimKobi@users.noreply.github.com>
2024-09-13 00:45:18 +0100

    section done

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -16,39 +16,6 @@ const Team = () => {
             CEO
           </p>
         </div>
-        <div className="md:w-1/4">
-          <img
-            src="/assets/img-blank.PNG"
-            alt=""
-            className="md:h-80 h-60 w-full rounded-b-3xl rounded-tl-[50%] object-cover bg-[#cbcbc9]"
-          />
-          <p className="text-center pt-4 md:text-2xl font-semibold">Anjas</p>
-          <p className="text-center pt-4 md:text-base text-sm text-[#0c4f37] font-semibold">
-            Founder
-          </p>
-        </div>
-        <div className="md:w-1/4">
-          <img
-            src="/assets/img-blank.PNG"
-            alt=""
-            className="md:h-80 h-60 w-full rounded-b-3xl rounded-tl-[50%] object-cover bg-[#cbcbc9]"
-          />
-          <p className="text-center pt-4 md:text-2xl font-semibold">Anjas</p>
-          <p className="text-center pt-4 md:text-base text-sm text-[#0c4f37] font-semibold">
-            Developer
-          </p>
-        </div>
-        <div className="md:w-1/4">
-          <img
-            src="/assets/img-blank.PNG"
-            alt=""
-            className="md:h-80 h-60 w-full rounded-b-3xl rounded-tl-[50%] object-cover bg-[#cbcbc9]"
-          />
-          <p className="text-center pt-4 md:text-2xl font-semibold">Anjas</p>
-          <p className="text-center pt-4 md:text-base text-sm text-[#0c4f37] font-semibold">
-            Designer
-          </p>
-        </div>
       </section>
     </main>
   );
